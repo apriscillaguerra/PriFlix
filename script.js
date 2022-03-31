52141f6e2ccf65961867446c98e3aaf495cfd0fb
Priscilla <guerra.priscilla@hotmail.com>
2022-03-31 08:44:50 -0300

    Correções

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//https://i.pinimg.com/originals/02/ea/19/02ea196f76a2fd03bee49ce7d61b344d.jpg
+
 var filmes = []
 var filme = {}
 
@@ -15,30 +17,30 @@ function adicionar() {
 
     if (!imagem.endsWith('.jpg' || '.jpeg' || '.png') || titulo == '') {
         alert('Dados incompletos! Titulo pendente ou URL diferente de JPG, JPEG ou PNG.')
-    } else {
-        filmes.push(construirLista(titulo, imagem))
+        return
     }
+
+    filmes.push(construirLista(titulo, imagem))
+
 
     document.querySelector('#endereco').value = ''
     document.querySelector('#titulo-filme').value = ''
-
-    console.log(imagem)
-    console.log(titulo)
 
     listar()
 }
 
 function listar() {
-    var catalogoFilme = document.querySelector('.imagem-filme')
+    var catalogoFilme = document.querySelector('.filme')
+    var listaFilmes = []
 
     for (var i in filmes) {
-        catalogoFilme.innerHTML += '<img class="banner-filme" src="' + filmes[i].imagem + '"><p class="nome-filme">' + filmes[i].titulo + '</p>'
+        listaFilmes.push('<div class="imagem-filme"><img class="banner-filme" src="' + filmes[i].imagem + '"><p class="nome-filme">' + filmes[i].titulo + '</p></div>')
     }
-
+    catalogoFilme.innerHTML = listaFilmes
 }
 
 function remover() {
-    var catalogo = document.querySelector('.imagem-filme')
+    var catalogo = document.querySelector('.filme')
 
     for (var i = filmes.length - 1; i >= 0; i--) {
         filmes.pop()
